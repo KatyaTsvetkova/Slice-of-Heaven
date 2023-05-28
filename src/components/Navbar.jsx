603cbf702d10ea83bbbb7c341abb32e286e8de12
KatyaTsvetkova <87665385+KatyaTsvetkova@users.noreply.github.com>
2023-05-28 21:16:18 +0300

    Add ReadMe

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,11 +10,17 @@ const Navbar = () => {
   const toggleNavbar =()=>{
     setOpenLinks(!openLinks)
   }
-
+  const handleClick = () => {
+    window.location.href = '/';
+  };
   return (
     <div className='navbar'>
       <div className='leftSide' id={openLinks ? "open" : "close"}>
-        <img src={Logo} alt="" />
+        <img 
+        src={Logo}
+        alt="logo" 
+        className='logo'
+        onClick={handleClick}/>
         <div className="hiddenLinks">
         <Link to="/"> Home </Link>
         <Link to="/menu"> Menu </Link>
